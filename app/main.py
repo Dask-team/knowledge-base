badb31b1f4341cea6e9c8439d23f6ef947d426f9
ryankwondev <kznm.develop@gmail.com>
2024-02-12 21:32:11 +0900

    keywird

--- app/main.py
+++ app/main.py
@@ -110,6 +110,7 @@ class Content(BaseModel):
 @app.post("/extract-keywords/")
 def extract_keywords(content: Content):
     """Extract keywords extract_keywords"""
+    print(content.content)
     return extract_keywords(str(content.content))
 
 
